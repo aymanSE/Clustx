983342ca66d314bb7a879dea918572176c19eb3e
aymanSE <61785906+aymanSE@users.noreply.github.com>
2023-03-17 00:26:13 +0300

    reports controller(dont ask why i forgot it)

--- Clust/app/Controllers/Http/ReportsController.ts
+++ Clust/app/Controllers/Http/ReportsController.ts
@@ -1,3 +1,75 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {schema,rules} from '@ioc:Adonis/Core/Validator'
+import Report from 'App/Models/Report'
 
-export default class ReportsController {}
+export default class ReportsController {
+
+    public async get(/*ctx: HttpContextContract*/){
+        var result = Report.all()
+        return result
+    }
+
+    public async getById(ctx: HttpContextContract){
+        var id= ctx.params.id
+        var result = Report.findOrFail(id)
+        return result
+    }
+
+
+    public async create(ctx: HttpContextContract){
+       const newSchema= schema.create({
+        description: schema.string(),
+        event_id: schema.number([
+            rules.exists({
+                table: 'events',
+                column:'id'
+            }),
+        ]),
+       
+       })
+       var fields= await ctx.request.validate({schema: newSchema})
+       var report= new Report()
+       report.description= fields.description
+       report.eventId=fields.event_id
+        var result= await report.save()
+       return result
+    }
+
+    public async update(ctx: HttpContextContract){
+        const newSchema= schema.create({
+            description: schema.string(),
+            event_id: schema.number([
+                rules.exists({
+                    table: 'events',
+                    column:'id'
+                }),
+            ]),
+            id: schema.number()
+
+           })
+           var fields= await ctx.request.validate({schema: newSchema})
+           var report= await Report.findOrFail(fields.id)
+           report.description= fields.description
+           report.eventId=fields.event_id
+            var result= await report.save()
+           return result
+    }
+
+    public async destroy(ctx: HttpContextContract){
+        try{
+            var id = ctx.params.id;
+            var  report = await  Report.findOrFail(id);
+            try{
+            await  report.delete();
+            return { message: "The  report has been deleted!" }
+            }
+            catch(e: unknown){
+              return  { message: "Cannot delete a used foreign key :(" }
+            }
+            }
+            catch(e:unknown){
+                return { message: "Report not found :(" }
+            }
+    }
+
+}
